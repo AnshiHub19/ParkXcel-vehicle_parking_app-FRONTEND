5fff6e6eb5855d3105067457be8ad07d50b630b8
23f3001392 <23f3001392@ds.study.iitm.ac.in>
2025-12-26 00:51:59 +0530

    fix register api for production

--- src/views/Register_P.vue
+++ src/views/Register_P.vue
@@ -55,8 +55,8 @@ export default {
       this.error = "";
       this.success = "";
       try {
-        const res = await axios.post("http://127.0.0.1:5000/api/register", {
-          username: this.username,
+        axios.post("https://parkxcel-vehicle-parking-app-backend.onrender.com/api/register", {
+  
           email: this.email,
           password: this.password,
         });
